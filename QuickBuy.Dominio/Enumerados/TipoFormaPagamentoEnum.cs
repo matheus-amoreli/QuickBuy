67a56e5627edbe944235581f34ff22c51cb692cd
Matheus Amoreli Silveira <matheus.silveira@unidesk.com.br>
2020-03-17 16:39:09 -0300

    Criação de Entidades

---
+++ QuickBuy.Dominio/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickBuy.Dominio.Entidades.Enumerados
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido = 0,
+        Boleto = 1,
+        CartaoDeCredito = 2,
+        Deposito = 3
+    }
+}
